9c88111d5d62c873a6f34ea8cec756c4249e8424
HoangHippo <hoanghippo17@gmail.com>
2023-01-11 23:36:23 +0700

    server

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -7,4 +7,16 @@ const verifyToken = (req,res,next) =>{
 
     if(!token)
     return res.status(401).json({success:false, message:'Access token not found'})
+
+    try {
+        const decoded = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+        // decoded same userID
+        req.userId = decoded.userId
+        next()
+    } catch (error) {
+        console.log(error)
+        return res.status(403).json({success:false, message:'Invalid token'})
+    }
 }
+ 
+module.exports = verifyToken
